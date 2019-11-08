8908d15138611fe0897e1ca9591aea9888aea229
sunghyun01 <17ubi23@gmail.com>
2019-11-08 23:34:53 +0900

    함수정리중

--- namkang/test.php
+++ namkang/test.php
@@ -1,26 +1,41 @@
 <?
-include("lunch.php");  
+include("lunch.php");
 header('Content-type: application/json');
 
 if($_GET['day'] == "T"){
     $lunch = get_lunch_today($_GET['code']);
+    lunchFunc();
 }else{
     $lunch = get_lunch_tomorrow($_GET['code']);
+    lunchFunc();
+    //?day=Y&code=B100000403
 }
 
 
-$exp = split('\n', $lunch);
+function lunchFunc(){
+    $exp = split('\n', $lunch);
+    $arr = array();
+    $arr2 = array();
+    $arr['version'] = "2.0";
+    $imp = implode('', $exp);
+    $str = str_replace("\\", "\n", $imp);
+    $arr2['simpleText']['text'] = "[중식]".$str;
+    $arr['template']['outputs'] = array($arr2);
 
-$arr = array();
-$arr2 = array();
-$arr['version'] = "2.0";
-$imp = implode('', $exp);
-$str = str_replace("\\", "\n", $imp);
-$arr2['simpleText']['text'] = "[중식]".$str;
-$arr['template']['outputs'] = array($arr2);
+    $res =  json_encode($arr);
 
+    print_r($res);
+}
+function simpleTextFunc($text){
+    $arr = array();
+    $arr2 = array();
+    $arr['version'] = "2.0";
+    $imp = implode('', $exp);
+    $str = str_replace("\\", "\n", $imp);
+    $arr2['simpleText']['text'] = $text;
+    $arr['template']['outputs'] = array($arr2);
+    $res =  json_encode($arr);
 
-$res =  json_encode($arr);
-
-print_r($res);
+    print_r($res);
+}
 ?>

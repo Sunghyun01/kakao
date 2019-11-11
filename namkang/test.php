39cafff50a2409fe4e66f65dbf35c920e7921c81
Sunghyun01 <37784262+Sunghyun01@users.noreply.github.com>
2019-11-11 23:53:14 +0900

    bug fix

--- namkang/test.php
+++ namkang/test.php
@@ -4,11 +4,32 @@ header('Content-type: application/json');
 
 if($_GET['day'] == "T"){
     $lunch = get_lunch_today($_GET['code']);
-    lunchFunc();
+    $exp = split('\n', $lunch);
+    $arr = array();
+    $arr2 = array();
+    $arr['version'] = "2.0";
+    $imp = implode('', $exp);
+    $str = str_replace("\\", "\n", $imp);
+    $arr2['simpleText']['text'] = "[중식]".$str;
+    $arr['template']['outputs'] = array($arr2);
+
+    $res =  json_encode($arr);
+
+    print_r($res);
 }else{
     $lunch = get_lunch_tomorrow($_GET['code']);
-    lunchFunc();
-    //?day=Y&code=B100000403
+    $exp = split('\n', $lunch);
+    $arr = array();
+    $arr2 = array();
+    $arr['version'] = "2.0";
+    $imp = implode('', $exp);
+    $str = str_replace("\\", "\n", $imp);
+    $arr2['simpleText']['text'] = "[중식]".$str;
+    $arr['template']['outputs'] = array($arr2);
+
+    $res =  json_encode($arr);
+
+    print_r($res);
 }
 
 

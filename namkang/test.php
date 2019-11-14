30a6125d014871126c8f5ddd0968bba9906d90b8
Sunghyun01 <37784262+Sunghyun01@users.noreply.github.com>
2019-11-14 16:34:13 +0900

    Update test.php

--- namkang/test.php
+++ namkang/test.php
@@ -3,39 +3,21 @@ include("lunch.php");
 header('Content-type: application/json');
 
 $code = 'B100000403';
-if($_GET['day'] == "T"){
+$weatherCode = '1162010200';
+if($_GET['day'] == "TL"){
     $lunch = get_lunch_today(code);
-    $exp = split('\n', $lunch);
-    $arr = array();
-    $arr2 = array();
-    $arr['version'] = "2.0";
-    $imp = implode('', $exp);
-    $str = str_replace("\\", "\n", $imp);
-    $arr2['simpleText']['text'] = "[중식]".$str;
-    $arr['template']['outputs'] = array($arr2);
-
-    $res =  json_encode($arr);
-
-    print_r($res);
-}else{
+    changeType($lunch)
+}elseif($_GET['day'] == "YL")){
     $lunch = get_lunch_tomorrow(code);
-    $exp = split('\n', $lunch);
-    $arr = array();
-    $arr2 = array();
-    $arr['version'] = "2.0";
-    $imp = implode('', $exp);
-    $str = str_replace("\\", "\n", $imp);
-    $arr2['simpleText']['text'] = "[중식]".$str;
-    $arr['template']['outputs'] = array($arr2);
-
-    $res =  json_encode($arr);
-
-    print_r($res);
+    changeType($lunch)
+}elseif($_GET['code'] == "TW")){
+   $weather = get_weather_today($weatherCode);
+   changeType($weather)
 }
 
 
-function lunchFunc(){
-    $exp = split('\n', $lunch);
+function changeType($changeJson){
+    $exp = split('\n', $$changeJson);
     $arr = array();
     $arr2 = array();
     $arr['version'] = "2.0";

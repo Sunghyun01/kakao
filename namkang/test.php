3d59bd23c191c4cb1f5350a4b6e0ddee77c4d47e
sunghyun01 <17ubi23@gmail.com>
2019-11-18 22:56:15 +0900

    '버그수정'

--- namkang/test.php
+++ namkang/test.php
@@ -1,23 +1,28 @@
 <?
 include("lunch.php");
+include('weather.php');
+
 header('Content-type: application/json');
 
 $code = 'B100000403';
 $weatherCode = '1162010200';
 if($_GET['code'] == "TL"){
     $lunch = get_lunch_today(code);
-    changeType($lunch)
-}elseif($_GET['code'] == "YL")){
+    changeType($lunch);
+}elseif($_GET['code'] == "YL"){
     $lunch = get_lunch_tomorrow(code);
-    changeType($lunch)
-}elseif($_GET['code'] == "TW")){
+    changeType($lunch);
+}elseif($_GET['code'] == "TW"){
    $weather = get_weather_today($weatherCode);
-   simpleTextFunc($weather)
+   simpleTextFunc($weather);
+}elseif($_GET['code'] == "YW"){
+   $weather = get_weather_tomorrow($weatherCode);
+   simpleTextFunc($weather);
 }
 
 
 function changeType($changeJson){
-    $exp = split('\n', $$changeJson);
+    $exp = split('\n', $changeJson);
     $arr = array();
     $arr2 = array();
     $arr['version'] = "2.0";
@@ -31,6 +36,7 @@ function changeType($changeJson){
     print_r($res);
 }
 function simpleTextFunc($weather){
+    $exp = split('\n', $weather);
     $arr = array();
     $arr2 = array();
     $arr['version'] = "2.0";
